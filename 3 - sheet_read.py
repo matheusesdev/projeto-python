68039ce87fb32f27073c3de867c872e95b0ab0d3
Matheus <matheuses18@icloud.com>
2024-01-07 12:23:38 -0300

    update

--- 3 - sheet_read.py
+++ 3 - sheet_read.py
@@ -3,4 +3,14 @@ from openpyxl import load_workbook
 # 1 - Lê pasta de trabalho e planilha
 wb = load_workbook("data/pivot_table.xlsx")
 sheet = wb["Relatório"]
-print(sheet)
+
+# 2 Acessando um valor específico
+print(sheet["A3"].value)
+print(sheet["B3"].value)
+
+# 3  Interando valores por meio de loop
+for i in range (2,6):
+    ano = sheet["A%s" %i].value
+    am = sheet["B%s" %i].value
+    bt = sheet["C%s" %i].value
+    print("{0} o Aston Martin vendeu {1} e o Bentley vendeu {2}".format(ano, am, bt))
